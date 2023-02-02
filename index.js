6a60a814e201aa8a503ad8b30d877e66dd2c9c2f
Sertaç Kocagil <srtc.kcgl@gmail.com>
2023-02-02 18:16:50 +0300

    Son gorev de tamamlandi

--- index.js
+++ index.js
@@ -13,10 +13,54 @@
     - Kisi örneklerine `.toString()` metodu ekleyin:
         + Bu `isim` ve `yas` i içeren bir string döndürmelidir Örnek: "Mary, 50"
 */
+// function Kullanici(isim) {
+//   this.isim = isim;
+//   this.yoneticiMi = false;
+// }
 
-function Kisi(/* kodlar buraya */) {
-  /* kodlar buraya */
-}
+// let adam = new Kullanici("İhsan");
+// let adam3 = new Kullanici("Melis");
+// // alert(kullanici.isim); // İhsan
+// // alert(kullanici.yoneticiMi); // false
+// console.log(adam.isim);
+
+// let adam2 = {
+//   isim: "Emre",
+//   yoneticiMi: true,
+//   yazdir: function(){
+//     return "ismim :" + this.isim;
+//   }
+// }
+// adam2.isim= "Ahmet";
+// console.log(adam2.yazdir());
+// console.log(adam3.yazdir());
+
+
+function Kisi(isim, yas) {
+  this.isim = isim;
+  this.yas = yas;
+  this.mide =[];
+  this.ye = function(yemek){
+    if(this.mide.length<10){
+    return this.mide.push(yemek);
+    }
+  }
+  this.bosalt = function(){ 
+    this.mide =[];
+  }
+ 
+  this.toString = function() {
+    return this.isim + ', ' + this.yas;
+  };
+} 
+
+
+let name1 = new Kisi("İhsan", 40);
+name1.ye("Patates")
+name1.ye("Ekmek")
+name1.toString()
+
+console.log(name1.mide)
 
 
 /*
@@ -35,18 +79,52 @@ function Kisi(/* kodlar buraya */) {
         +  "x milde benzinim bitti!" x değişkeni `odometer` daki sayı olmalıdır.
 */
 
-function Araba(/* kodlar buraya */) {
-  /* kodlar buraya */
+function Araba(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+
+  this.fill = function(gallons) {
+    this.tank += gallons;
+  };
+
+  this.drive = function(distance) {
+    let gerekenYakit = distance / this.milesPerGallon;
+    if (gerekenYakit > this.tank) {
+      console.log(`${this.odometer} milde benzinim bitti!`);
+      } else {
+  this.odometer += distance;
+      this.tank -= gerekenYakit;
+    };
+  }
 }
+
+
+
+
+
+
 
 
 /* 
   GÖREV 3
   Kendi cümlelerinizle "this" kelimesinin 4 prensibini açıklayın:
-  1. 
-  2. 
-  3. 
-  4. 
+
+  1.Global scope üzerinde kullanıldığında window/console nesnesi olur.
+  
+  2.Implicit(Örtük) Binding = 
+  En yaygın kullanımı bu şekilde olur. Bir fonksiyonu çağırmak için kullanılır. 
+  
+  3. New Binding = 
+  Constructor fonksiyonu ile yeni bir obje tanımlarken kullanılır. Bu objenin properties değerlerini girerken ve içindeki fonksiyonları tanımlarken kullanırız. 
+
+  4. Explicit (Açık) Binding =
+  call ve apply ile birlikte bu yöntem kullanılır. -Benim anladığım ve şimdiye kadar gördüğüm kadarıyla- daha önce tanımladığımız bir objeye dışarıdan müdahale ederek 
+  bu objenin propertylerini değiştirmek için kullanırız. 
+
+
+
 */
 
 
